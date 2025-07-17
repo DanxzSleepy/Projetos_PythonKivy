e47e173e22abf52c8f390d86336a2c03e10dab75
DanxzSleepy <wolfdangalaxy2@gmail.com>
2025-07-17 17:30:36 -0300

    Habilitar desenho de linhas ao tocar em movimento
    Adicionado o método on_touch_move para permitir que os usuários desenhem linhas contínuas ao mover o dedo ou o mouse. Também corrigimos a inicialização de cores em on_touch_down.

--- Paint/main.py
+++ Paint/main.py
@@ -6,9 +6,13 @@ from kivy.graphics import Color, Ellipse, Line
 class MeuPaintWidget(Widget):
     def on_touch_down(self, touch):
         with self.canvas:
-            color = (1, 1, 0)
+            Color(1, 1, 0)
             d = 30.
             Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+            touch.ud['line'] = Line(points=(touch.x, touch.y))
+
+    def on_touch_move(self, touch):
+        touch.ud['line'].points += [touch.x, touch.y]
 
 
 class MeuPaintApp(App):
