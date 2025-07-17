ce33bf5fa93d69020dbad501f55dc9a075bc5454
DanxzSleepy <wolfdangalaxy2@gmail.com>
2025-07-17 17:51:30 -0300

    Adicionar botão limpar para redefinir a tela de pintura
    Introduz um botão "Limpar" na interface do aplicativo de pintura. Ao ser pressionado, ele limpa a tela de desenho, permitindo que os usuários reiniciem sem precisar reiniciar o aplicativo.

--- Paint/main.py
+++ Paint/main.py
@@ -1,6 +1,7 @@
 from random import random
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
 from kivy.graphics import Color, Ellipse, Line
 
 
@@ -17,8 +18,18 @@ class MeuPaintWidget(Widget):
 
 
 class MeuPaintApp(App):
+
     def build(self):
-        return MeuPaintWidget()
+        parent = Widget()
+        self.painter = MeuPaintWidget()
+        clearbtn =  Button(text='Clear')
+        clearbtn.bind (on_release=self.clear_canvas)
+        parent.add_widget(self.painter)
+        parent.add_widget(clearbtn)
+        return parent
+
+    def clear_canvas(self, obj):
+        self.painter.canvas.clear()
 
 if __name__ == '__main__':
     MeuPaintApp().run()
